9ce850cfb41fd8d0ca9d4549b0bf2f98cfd5553a
DorianGosse <58377140+DorianGosse@users.noreply.github.com>
2020-06-21 17:44:34 -0500

    Home text boxs
    These should be components however, updated them in html is display required text and added buttons/css to the text div elements

--- src/pages/homePage/home.jsx
+++ src/pages/homePage/home.jsx
@@ -5,11 +5,13 @@ import TextBox from "../../components/textbox/TextBox";
 import NavBar from "../../components/navBar/NavBar";
 import Footer from "../../components/footer/Footer";
 
+import { Button } from "../../components/interactable/button/button";
+
 export default function home() {
   return (
     <div className="home-page">
       <NavBar />
-      <div className="textbox">
+      <div className="textbox" id="who-we-are">
         <TextBox />
       </div>
       <img
@@ -21,21 +23,29 @@ export default function home() {
         <h2>What do we do at WBD?</h2>
         <p>Check out our past work and services</p>
       </div>
-      <div className="services-text">
-        <div className="services-header">
+      <div className="text" id="services-textbox">
+        <div className="textbox-header" id="services-header">
           <h3>Our Services Include:</h3>
         </div>
         <p>
-          <bold>Landing Pages: </bold> For those business owners looking for the
-          perfect online business card
+          <bold>Landing Pages: </bold>
+          <br />
+          <br /> For those business owners looking for the perfect online
+          business card
         </p>
         <p>
-          <bold>Multi-Page Sites: </bold> For those looking to offer their
-          customers the full experience of what you have to offer
+          <bold>Multi-Page Sites: </bold>
+          <br />
+          <br />
+          For those looking to offer their customers the full experience of what
+          you have to offer
         </p>
         <p>
-          <bold>Interactive and Functional:</bold>We offer dynamic,
-          interactable, functional sites to serve all your needs
+          <bold>Interactive and Functional:</bold>
+          <br />
+          <br />
+          We offer dynamic, interactable, functional sites to serve all your
+          needs
         </p>
         <p>
           Web Byte Design aims to provide you with a solution to fit your
@@ -43,11 +53,15 @@ export default function home() {
           Whether you're a blogger, small business, video maker, or anything in
           between we've got a solution for you!
         </p>
+        <div className="buttons">
+          <Button>Learn More</Button>
+        </div>
       </div>
 
-      <div className="protfolio-text">
-        {/* need a learn more button here */}
-        <h3>Want to See out Work?</h3>
+      <div className="text" id="protfolio-textbox">
+        <div className="textbox-header" id="portfolio-header">
+          <h3>Want to See out Work?</h3>
+        </div>
         <p>
           We don't expect you to take our word for how well we do our jobs
           becuase anyone can write some html. So see for yourself! You can find
@@ -57,8 +71,10 @@ export default function home() {
         <a className="JTSD-link" href="journeyToSoftwareDevelopment.com">
           Journey to Software Development
         </a>
-        {/* details button here  */}
-        <p></p>
+        <div className="buttons">
+          <Button>Details</Button>
+          <Button>See More Work</Button>
+        </div>
       </div>
       <img
         className="services-img"
